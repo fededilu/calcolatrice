384d5308e7a99482dde7a7e4066dd9b3ae88b607
plaias <federicodiluzio@hotmail.it>
2022-11-26 17:45:02 +0100

    da finire

--- src/main/java/com/example/calcolatrice/HelloController.java
+++ src/main/java/com/example/calcolatrice/HelloController.java
@@ -1,14 +1,89 @@
 package com.example.calcolatrice;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control
 
 public class HelloController {
+
+    @FXML
+    private Button B1;
+
+    @FXML
+    private Button B2;
+
+    @FXML
+    private Button B3;
+
+    @FXML
+    private Button B4;
+
+    @FXML
+    private Button B5;
+
+    @FXML
+    private Button B6;
+
+    @FXML
+    private Button B7;
+
+    @FXML
+    private Button B8;
+
+    @FXML
+    private Button B9;
+
+    @FXML
+    void button1Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button2Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button3Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button4Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button5Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button6Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button7Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button8Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button9Clicked(ActionEvent event) {
+
+    }
+
+    /*
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    }*/
 }
